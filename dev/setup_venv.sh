7d68c4c81c3808edbdfc000f611ac0c9b5993611
Daniel Baena <dbaena@alea-soluciones.com>
2019-10-28 17:47:07 +0100

    [FEATURE] Add dev depenencies installer script

---
+++ dev/setup_venv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "You need to be on a virtual environment to install dev dependencies"
+    :
+else
+    echo
+    echo "----------------------------------------------------------------------"
+    echo "Installing Dev Dependencies"
+    echo "----------------------------------------------------------------------"
+    echo
+    python setup.py develop
+    pip install pip --upgrade
+    pip install -r requirements.txt --upgrade
+fi
