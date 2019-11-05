f8566f03c9b016454e8c1f85f5c0a57bab8a12ba
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 13:53:00 +0100

    [ENHANCEMENT] Improve hooks installation output
    - Use proper commands in hooks

--- scripts/git_hooks.sh
+++ scripts/git_hooks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copy git hooks to your current git repository
+
+source scripts/shared_utils/output.sh
 
 echo
 echo "----------------------------------------------------------------------"
@@ -7,11 +8,11 @@ echo "Copying git hooks"
 echo "----------------------------------------------------------------------"
 echo
 
-echo "Current project: $(basename $(pwd))"
+echo "Current project: $(info $(basename $(pwd)))"
 echo
 for hook in config/git-hooks/*; do
-  echo "--> Copying hook \"$(basename $hook)\""
+  echo "--> Copying hook \"$(warning $(basename $hook))\""
   cp $hook .git/hooks/$(basename $hook)
 done
 echo
-echo "Every git hook was copied fine"
+info "Every git hook was copied fine"
