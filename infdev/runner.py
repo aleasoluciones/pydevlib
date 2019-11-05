4134148b4c3fe8b09bd623d3e35b2fdb4d8a24b2
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 12:03:34 +0100

    [FEATURE] Improve runner capabilities

--- infdev/runner.py
+++ infdev/runner.py
@@ -6,9 +6,10 @@ import traceback
 
 
 SCRIPT_TYPES_PATH = {
-    'git_hooks': "tools/configure_git_hooks.sh",
+    'git_hooks': "scripts/git_hooks.sh",
     'linter': "scripts/python_linter.sh",
-    'formatter': "scripts/python_formatter.sh",
+    'yapf_formatter': "scripts/python_yapf_formatter.sh",
+    'black_formatter': "scripts/python_black_formatter.sh",
     'search_focused_specs': "scripts/search_focused_specs.sh",
     'unit_tests': "scripts/unit_tests.sh",
     'factory_tests': "scripts/factory_tests.sh",
@@ -22,19 +23,32 @@ SCRIPT_TYPES_PATH = {
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--type', action='store', required=False, help='Script type')
+    parser.add_argument('-r', '--run', action='store', required=False, help='Run script')
     parser.add_argument('-l', '--list', action='store_true', required=False, help=' List dev script types')
-    args = parser.parse_args()
-    if args.type:
-        script_type = SCRIPT_TYPES_PATH[args.type]
-        call(script_type)
+    args, unknown_args = parser.parse_known_args()
+
     if args.list:
+        print("The available scripts list is:\n")
         for script_type in SCRIPT_TYPES_PATH.keys():
-            print(script_type)
+            print(f"- {script_type}")
+        print("")
+        return
+
+    if not args.run and unknown_args:
+        args.run = unknown_args[0]
+
+    try:
+        script_type = SCRIPT_TYPES_PATH[args.run]
+        call([script_type, *unknown_args[1:]])
+    except KeyError:
+        print("There's no such script to run :( The available scripts list is:\n")
+        for script_type in SCRIPT_TYPES_PATH.keys():
+            print(f"- {script_type}")
+        print("")
 
 
 if __name__ == '__main__':
     try:
         main()
-    except Exception as exc:
+    except Exception:
         print(traceback.print_exc())
