ba8b17e538dfa2fb77428f5618fbac40dea0815f
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 15:05:56 +0100

    [REFACTOR] Improve readability for scripts path

--- infdev/runner.py
+++ infdev/runner.py
@@ -5,23 +5,22 @@ import argparse
 import traceback
 import os
 
-
 current_file_path = os.path.dirname(os.path.abspath(__file__))
-
+scripts_path = f"{current_file_path}/../scripts"
 
 SCRIPT_TYPES_PATH = {
-    "git_hooks": f"{current_file_path}/../scripts/git_hooks.sh",
-    "linter": f"{current_file_path}/../scripts/python_linter.sh",
-    "yapf_formatter": f"{current_file_path}/../scripts/python_yapf_formatter.sh",
-    "black_formatter": f"{current_file_path}/../scripts/python_black_formatter.sh",
-    "focused_specs": f"{current_file_path}/../scripts/search_focused_specs.sh",
-    "unit_tests": f"{current_file_path}/../scripts/unit_tests.sh",
-    "factory_tests": f"{current_file_path}/../scripts/factory_tests.sh",
-    "integration_tests": f"{current_file_path}/../scripts/integration_tests.sh",
-    "functional_tests": f"{current_file_path}/../scripts/functional_tests.sh",
-    "acceptance_tests": f"{current_file_path}/../scripts/acceptance_tests.sh",
-    "local_tests": f"{current_file_path}/../scripts/local_tests.sh",
-    "all_tests": f"{current_file_path}/../scripts/all_tests.sh",
+    "git_hooks": f"{scripts_path}/git_hooks.sh",
+    "linter": f"{scripts_path}/python_linter.sh",
+    "yapf_formatter": f"{scripts_path}/python_yapf_formatter.sh",
+    "black_formatter": f"{scripts_path}/python_black_formatter.sh",
+    "focused_specs": f"{scripts_path}/search_focused_specs.sh",
+    "unit_tests": f"{scripts_path}/unit_tests.sh",
+    "factory_tests": f"{scripts_path}/factory_tests.sh",
+    "integration_tests": f"{scripts_path}/integration_tests.sh",
+    "functional_tests": f"{scripts_path}/functional_tests.sh",
+    "acceptance_tests": f"{scripts_path}/acceptance_tests.sh",
+    "local_tests": f"{scripts_path}/local_tests.sh",
+    "all_tests": f"{scripts_path}/all_tests.sh",
 }
 
 
