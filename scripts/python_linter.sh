0f9a69fc54633593ff31cc78427d2d79c37b4a2d
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 13:14:49 +0100

    [FEATURE] Add linter script
    - Add output script util for formatting

---
+++ scripts/python_linter.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+source scripts/shared_utils/output.sh
+
+echo
+echo "----------------------------------------------------------------------"
+echo "Running Python linter: pylint"
+echo "----------------------------------------------------------------------"
+echo
+
+EXIT_CODE=0
+
+if [ -z "$1"  ]; then
+    files=$(find . -type f -name "*.py" | grep -v 'spec')
+    if [[ $files ]]; then
+        info "Analizing PRODUCTION code..."
+        
+        output=$(pylint --rcfile config/.pylintrc ${files})
+        retcode=$?
+        exit_on_fatal_or_error $retcode "$output"
+    else
+        warning 'Not found python PRODUCTION code'
+        echo
+    fi
+
+    spec_files=$(find . -type f -name "*spec.py")
+    if [[ $spec_files ]]; then
+      info "Analizing SPECS code..."
+      output=$(pylint --rcfile config/.pylintrc-mamba ${spec_files})
+      retcode=$?
+      exit_on_fatal_or_error $retcode "$output"
+    else
+        warning 'Not found python SPECS code'
+        echo
+    fi
+
+elif [ $1 = "staged" ]; then
+  # https://git-scm.com/docs/git-status#_output
+  # M = modified A = added D = deleted R = renamed C = copied U = updated but unmerged
+  staged_files=$(git status --porcelain | grep "^[MCA]" | awk '$1~/^[MCA]/ && $2~/.py$/ {print $2}')
+
+  if [[ $staged_files ]]; then
+        source_files=$(echo $staged_files | grep -v 'spec')
+        if [[ $source_files ]]; then
+          info 'Analizing git staged PRODUCTION code...'
+          output=$(pylint --rcfile config/.pylintrc $source_files)
+          retcode=$?
+          exit_on_fatal_or_error $retcode "$output"
+        fi ;
+
+        spec_files=$(echo $staged_files | grep 'spec')
+        if [[ $spec_files ]]; then
+          info "Analizing git staged SPECS code..."
+          output=$(pylint --rcfile config/.pylintrc-mamba ${spec_files})
+          retcode=$?
+          exit_on_fatal_or_error $retcode "$output"
+        fi
+
+    else
+        warning 'Not found python staged files'
+        echo
+    fi
+fi
+
+exit $EXIT_CODE
---
+++ scripts/shared_utils/output.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+COLOR_FOR_BASH=\\e
+
+if (( $(uname) == 'Darwin' )); then
+    COLOR_FOR_BASH=\\x1B
+fi
+
+GREEN="$COLOR_FOR_BASH[92m"
+YELLOW="$COLOR_FOR_BASH[93m"
+RED="$COLOR_FOR_BASH[31m"
+NO_COLOR='\033[0m' # No Color
+
+function info(){
+  echo -e "${GREEN}$1${NO_COLOR}"
+}
+
+function warning(){
+  echo -e "${YELLOW}$1${NO_COLOR}"
+}
+
+function error(){
+  echo -e "${RED}$1${NO_COLOR}"
+}
+
+function force_exit(){
+    exit_code=$1
+    echo "Exit code:" $exit_code
+    exit $exit_code
+}
+
+function exit_on_fatal_or_error(){
+  FATAL_MESSAGE_ISSUED=1
+  ERROR_MESSAGE_ISSUED=2
+  retcode=$1
+  output=$2
+  is_fatal=$(($retcode & $FATAL_MESSAGE_ISSUED))
+  is_error=$(($retcode & $ERROR_MESSAGE_ISSUED))
+  undescartable_error=$(( $is_fatal || $is_error ))
+
+  if [ "$undescartable_error" -ne "0" ]; then
+    EXIT_CODE=$(($EXIT_CODE || $undescartable_error))
+    error "Error de linter: ${retcode}"
+    error "fatal and errors: "
+    fatals_and_errors=$(echo "$output" | grep -E "[EF][0-9]{4}")
+    echo "${fatals_and_errors}"
+    force_exit $EXIT_CODE
+    fi
+
+  echo "$output"
+  echo
+}
