f0c8f6347cfe02ed6e4ba2c099c5387d55183bf7
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 14:13:19 +0100

    [FEATURE] Improve paths in runner

--- infdev/runner.py
+++ infdev/runner.py
@@ -3,21 +3,25 @@
 from subprocess import call
 import argparse
 import traceback
+import os
+
+
+current_file_path = os.path.dirname(os.path.abspath(__file__))
 
 
 SCRIPT_TYPES_PATH = {
-    "git_hooks": "scripts/git_hooks.sh",
-    "linter": "scripts/python_linter.sh",
-    "yapf_formatter": "scripts/python_yapf_formatter.sh",
-    "black_formatter": "scripts/python_black_formatter.sh",
-    "focused_specs": "scripts/search_focused_specs.sh",
-    "unit_tests": "scripts/unit_tests.sh",
-    "factory_tests": "scripts/factory_tests.sh",
-    "integration_tests": "scripts/integration_tests.sh",
-    "functional_tests": "scripts/functional_tests.sh",
-    "acceptance_tests": "scripts/acceptance_tests.sh",
-    "local_tests": "scripts/local_tests.sh",
-    "all_tests": "scripts/all_tests.sh",
+    "git_hooks": f"{current_file_path}/../scripts/git_hooks.sh",
+    "linter": f"{current_file_path}/../scripts/python_linter.sh",
+    "yapf_formatter": f"{current_file_path}/../scripts/python_yapf_formatter.sh",
+    "black_formatter": f"{current_file_path}/../scripts/python_black_formatter.sh",
+    "focused_specs": f"{current_file_path}/../scripts/search_focused_specs.sh",
+    "unit_tests": f"{current_file_path}/../scripts/unit_tests.sh",
+    "factory_tests": f"{current_file_path}/../scripts/factory_tests.sh",
+    "integration_tests": f"{current_file_path}/../scripts/integration_tests.sh",
+    "functional_tests": f"{current_file_path}/../scripts/functional_tests.sh",
+    "acceptance_tests": f"{current_file_path}/../scripts/acceptance_tests.sh",
+    "local_tests": f"{current_file_path}/../scripts/local_tests.sh",
+    "all_tests": f"{current_file_path}/../scripts/all_tests.sh",
 }
 
 
