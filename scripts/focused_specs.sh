6f8eaf02a386414d213507e0b65628cb51cbd273
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 17:30:18 +0100

    [FEATURE] Add proper paths in focused spec exclude dir options

--- scripts/focused_specs.sh
+++ scripts/focused_specs.sh
@@ -9,7 +9,7 @@ echo "Searching for Focused Specs"
 echo "----------------------------------------------------------------------"
 echo
 
-FOCUSED_SPECS=$(grep -rn --exclude-dir=dev 'with fit\|with fcontext\|with fdescribe\|with fdescription' .)
+FOCUSED_SPECS=$(grep -rn --exclude-dir={$scripts_path,scripts} --exclude=search_focused_specs.sh 'with fit\|with fcontext\|with fdescribe\|with fdescription' .)
 if [ -z "$FOCUSED_SPECS" ]; then
     info "Focused specs NOT found"
     FOCUS_RETCODE=0
