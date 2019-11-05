706e6c7242ba769e041687c0344f88cbc422eaa1
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-05 13:50:49 +0100

    [FEATURE] Add unit, integration and acceptance tests scripts

---
+++ scripts/acceptance_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source scripts/shared_utils/clean_pyc_files.sh
+
+echo
+echo "----------------------------------------------------------------------"
+echo "Running Acceptance Specs"
+echo "----------------------------------------------------------------------"
+echo
+
+SPEC_PATTERN="acceptance_specs"
+SEARCH_PATH="."
+IGNORE_PATH="systems"
+SPEC_FILES=$(find $SEARCH_PATH -maxdepth 2 -type d -name $SPEC_PATTERN | grep -v $IGNORE_PATH)
+
+if [ -z "$1"  ]; then
+    FORMATTER="progress"
+    mamba -f $FORMATTER $SPEC_FILES;
+elif [ $1 = "doc" ]; then
+    FORMATTER="documentation"
+    mamba -f $FORMATTER $SPEC_FILES;
+elif [ $1 = "debug" ]; then
+    for f in $SPEC_FILES; do
+        echo "$f ..."
+        mamba $f
+    done;
+fi
+
+MAMBA_RETCODE=$?
+
+exit $MAMBA_RETCODE
