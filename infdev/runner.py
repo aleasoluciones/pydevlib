75639daf81fa2bb9d099be9a703a1ea234e8f086
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-06 23:27:34 +0100

    [INTERNAL] Add explanation for commands

--- infdev/runner.py
+++ infdev/runner.py
@@ -8,18 +8,19 @@ from subprocess import call
 
 MODULE_BASE_PATH = f"{os.path.dirname(os.path.abspath(__file__))}/.."
 COMMANDS = [
-    "git_hooks",
-    "linter",
-    "yapf_formatter",
-    "black_formatter",
-    "focused_specs",
-    "unit_tests",
-    "factory_tests",
-    "integration_tests",
-    "functional_tests",
-    "acceptance_tests",
-    "local_tests",
-    "all_tests",
+    "mambo -> Alias for mamba -f documentation",
+    "git_hooks -> Install pre-commit and pre-push git hooks",
+    "linter -> Run pylint for production and specs code",
+    "yapf_formatter -> Run YAPF to format python code",
+    "black_formatter -> Run Black to format python code",
+    "focused_specs -> Search focused specs across the code",
+    "unit_tests -> Run unit tests",
+    "factory_tests -> Run factory tests",
+    "integration_tests -> Run integration tests",
+    "functional_tests -> Run functional tests",
+    "acceptance_tests -> Run acceptance tests",
+    "local_tests -> Run all local tests (unit, factory and integration tests)",
+    "all_tests -> Run all tests but acceptance tests (unit, factory, integration and functional tests",
 ]
 
 
@@ -40,8 +41,8 @@ def run(script_name, without_paths=False):
 
 def infdev():
     print("The available commands list is:\n")
-    for command in COMMANDS:
-        print(f"- {command}")
+    for index, command in enumerate(COMMANDS):
+        print(f"{index + 1}) {command}")
     print("")
 
 
