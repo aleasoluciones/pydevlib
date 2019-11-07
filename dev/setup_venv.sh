c4359033f191ef853b38ce86941da690914b17f8
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-07 14:19:17 +0100

    [INTERNAL] Improve setup_venv script

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -2,7 +2,6 @@
 
 if [ -z "$VIRTUAL_ENV" ]; then
     echo "You need to be on a virtual environment to install dev dependencies"
-    :
 else
     echo
     echo "----------------------------------------------------------------------"
@@ -10,4 +9,12 @@ else
     echo "----------------------------------------------------------------------"
     echo
     python setup.py develop
+    for package in $(ls -d */); do pushd $package; if [ -e setup.py ]; then python setup.py develop; fi; popd; done
+    pip install pip --upgrade
+    if [ -e requirements.txt ]; then
+        pip install -r requirements.txt --upgrade
+    fi
+    if [ -e requirements-dev.txt ]; then
+        pip install -r requirements-dev.txt --upgrade
+    fi
 fi
