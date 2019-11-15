d062ee112833a83b0293b148cfa6d611f18a6fd0
Daniel Baena <dbaena@alea-soluciones.com>
2019-11-15 11:20:25 +0100

    [FEATURE] Make tornado functional spec helper more generic

--- infdev/functional_spec_helpers.py
+++ infdev/functional_spec_helpers.py
@@ -1,31 +1,17 @@
 import logging
-import base64
 
 from tornado import escape
 from tornado.testing import AsyncHTTPTestCase
 
 
 class TornadoFunctionalTest(AsyncHTTPTestCase):
-    API_USER_NAME = "an_user"
-    API_USER_PASSWORD = "a_password"
-    API_USER_NETWORKS = "lab"
-
     def __init__(
-        self,
-        auth_username=None,
-        auth_password=None,
-        debug=False,
-        application=None,
-        user_service=None,
-        **kwargs,
+        self, application, debug=False, **kwargs,
     ):
         super(TornadoFunctionalTest, self).__init__()
 
-        self.auth_username = auth_username if auth_username else self.API_USER_NAME
-        self.auth_password = auth_password if auth_password else self.API_USER_PASSWORD
-        self.debug = debug
         self.application = application
-        self.user_service = user_service
+        self.debug = debug
         self.extra_args = kwargs
 
     def _verbose_logger(self, value):
@@ -34,21 +20,9 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
 
     def setUp(self):
         AsyncHTTPTestCase.setUp(self)
-        self._user = self._add_user(
-            username=self.auth_username,
-            password=self.auth_password,
-            networks=self.API_USER_NETWORKS,
-        )
-        self._credentials = self._build_credentials(
-            username=self.auth_username, password=self.auth_password
-        )
 
     def tearDown(self):
-        self._remove_user(self.auth_username)
-        try:
-            AsyncHTTPTestCase.tearDown(self)
-        except Exception:
-            pass
+        AsyncHTTPTestCase.tearDown(self)
 
     def get_app(self):
         self.app = self.application.create(debug=self.debug, **self.extra_args)
@@ -60,7 +34,7 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
         if response.error:
             fields = ["code", "error", "effective_url", "body"]
             for field in fields:
-                print(f"{field}: {getattr(response, field}")
+                print(f"{field}: {getattr(response, field)}")
 
     def fetch(
         self,
@@ -72,10 +46,14 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
         allow_nonstandard_methods=False,
         **kwargs,
     ):
+        auth = kwargs.get("auth", None)
+        if not auth:
+            auth = self.extra_args.get("auth", None)
         self._verbose_logger(verbose)
         response = self._fetch_url(
             url,
             method=method,
+            auth=auth,
             body=body,
             headers=headers,
             allow_nonstandard_methods=allow_nonstandard_methods,
@@ -83,7 +61,7 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
         self._dump_when_error(response, verbose)
         return self._update_response_json_body(response)
 
-    def _fetch_url(self, url, method, body, headers, allow_nonstandard_methods):
+    def _fetch_url(self, url, method, auth, body, headers, allow_nonstandard_methods):
         if not headers:
             headers = {}
         headers.update(self._json_headers())
@@ -91,8 +69,8 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
         response = super(TornadoFunctionalTest, self).fetch(
             url,
             method=method,
-            auth_username=self.auth_username,
-            auth_password=self.auth_password,
+            auth_username=auth[0],
+            auth_password=auth[1],
             body=body,
             headers=headers,
             allow_nonstandard_methods=allow_nonstandard_methods,
@@ -121,37 +99,3 @@ class TornadoFunctionalTest(AsyncHTTPTestCase):
 
     def runTest(self):
         pass
-
-    def _add_user(self, username, password, networks):
-        self.user_service.add_user(
-            username=username, password=password, networks=networks, local_admin=True
-        )
-
-    def _remove_user(self, username):
-        self.user_service.remove_user(username)
-
-    def _build_credentials(self, username, password):
-        credentials = f"{username}:{password}"
-        encoded_credentials = base64.b64encode(credentials)
-        return encoded_credentials
-
-    def check_urls(
-        self,
-        urls,
-        allowed_status_codes,
-        method="GET",
-        body=None,
-        network=API_USER_NETWORKS,
-    ):
-        for url in urls:
-            endpoint = f"/{network}/{url}"
-            response = self.fetch(url=endpoint, method=method, body=body)
-            self._handle_response(response, allowed_status_codes)
-
-    def _handle_response(self, response, allowed_status_codes):
-        if response.code < 300:
-            return
-        if response.code == 405 or response.code not in allowed_status_codes:
-            error_text = f"Response error url: {response.effective_url} code: {response.code} error: {response.error}"
-            print("Error", error_text)
-            raise Exception(error_text)
