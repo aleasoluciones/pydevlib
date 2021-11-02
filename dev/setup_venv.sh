a886be83cf8e06d7d01f0ba235f6efc14d282599
luisddm <luisdedios@alea-soluciones.com>
2021-11-02 16:30:44 +0100

    [INTERNAL] Adapt project to new setup practices

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -3,18 +3,6 @@
 if [ -z "$VIRTUAL_ENV" ]; then
     echo "You need to be on a virtual environment to install dev dependencies"
 else
-    echo
-    echo "----------------------------------------------------------------------"
-    echo "Installing Dev Dependencies"
-    echo "----------------------------------------------------------------------"
-    echo
-    python setup.py develop
-    for package in $(ls -d */); do pushd $package; if [ -e setup.py ]; then python setup.py develop; fi; popd; done
-    pip install pip --upgrade
-    if [ -e requirements.txt ]; then
-        pip install -r requirements.txt --upgrade
-    fi
-    if [ -e requirements-dev.txt ]; then
-        pip install -r requirements-dev.txt --upgrade
-    fi
+    python -m pip install --upgrade pip wheel
+    python -m pip install --upgrade -e .
 fi
