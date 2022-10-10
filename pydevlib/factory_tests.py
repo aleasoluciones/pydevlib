31516df95f9124152cd67abe7a4c08d97aa88528
Alberto <acruz@alea-soluciones.com>
2022-10-10 13:38:53 +0200

    Revert "[FIX] Running factory_tests needs info about current working path"
    This reverts commit bb542714a920df0fff7bbfaeb7079f831dfce3fa.

--- pydevlib/factory_tests.py
+++ pydevlib/factory_tests.py
@@ -82,7 +82,6 @@ def _import_module(module_name):
 if __name__ == "__main__":
     # pylint: disable=broad-except
     try:
-        sys.path.append(os.getcwd())
         find_and_call_functions_from()
         sys.exit(0)
     except Exception as exc:
